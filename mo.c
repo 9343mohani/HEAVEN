0fe17a9a6cb168d28ade08b81237141216aaaaab
unknown <mohanimehra86@gmail.com>
2025-12-10 17:34:52 +0530

    new

---
+++ mo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[50] = "Hello";
+    char str2[] = " World!";
+    strcat(str1, str2);  // Append str2 to str1
+    printf("%s\n", str1);
+    return 0;
+}
