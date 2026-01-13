638eb4d5ff2353977692dabc03a22cb72b879dd5
unknown <mohanimehra86@gmail.com>
2026-01-13 11:01:27 +0530

    new

--- ab.c
+++ ab.c
@@ -17,3 +17,19 @@
 
 //     return 0;
 // }
+#include <stdio.h>
+
+// struct Shape {
+//     void (*draw)(void);
+// };
+
+// void drawCircle() {
+//     printf("Drawing a Circle\n");
+// }
+
+// int main() {
+//     struct Shape circle;
+//     circle.draw = drawCircle;
+//     circle.draw(); // abstraction in action
+//     return 0;
+// }
